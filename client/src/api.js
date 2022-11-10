0a679698799e38192fb51eee013f953807acdd80
Chris Seaman <christopherseaman1983@gmail.com>
2022-11-10 18:44:21 +0000

    update

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,4 @@
 const DEV_API_URL = "http://localhost:8080"; //"http://localhost:9000/.netlify/functions/api";
+const REAL_API_URL = "https://stomp-api.netlify.app/.netlify/functions/api";
 
-export const API_URL = DEV_API_URL;
+export const API_URL = REAL_API_URL;
